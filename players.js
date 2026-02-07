8ac1f9fe3b563d286f4f7d3a1f0578bfabb1594a
aranciarossa9 <aranciarossa9@gmail.com>
2026-02-07 14:52:56 +0100

    Add initial player data to players.js

---
+++ players.js
@@ -0,0 +1,59 @@
+// players.js
+const PLAYERS = [
+  {
+    id: 1,
+    name: "Lionel Messi",
+    foot: "SX",
+    nationality: "Argentina",
+    role: "ATT",
+    trophies: ["Champions League", "Mondiale", "Liga", "Ligue 1"],
+    height: 169,
+    careerGoals: 800,
+    image: "images/messi.jpg"
+  },
+  {
+    id: 2,
+    name: "Cristiano Ronaldo",
+    foot: "DX",
+    nationality: "Portogallo",
+    role: "ATT",
+    trophies: ["Champions League", "Mondiale", "Premier League", "Liga", "Serie A"],
+    height: 187,
+    careerGoals: 870,
+    image: "images/ronaldo.jpg"
+  },
+  {
+    id: 3,
+    name: "Kylian Mbappé",
+    foot: "DX",
+    nationality: "Francia",
+    role: "ATT",
+    trophies: ["Champions League", "Mondiale", "Ligue 1"],
+    height: 178,
+    careerGoals: 300,
+    image: "images/mbappe.jpg"
+  },
+  {
+    id: 4,
+    name: "Virgil van Dijk",
+    foot: "DX",
+    nationality: "Paesi Bassi",
+    role: "DIF",
+    trophies: ["Champions League", "Premier League"],
+    height: 193,
+    careerGoals: 60,
+    image: "images/vandijk.jpg"
+  },
+  {
+    id: 5,
+    name: "Manuel Neuer",
+    foot: "DX",
+    nationality: "Germania",
+    role: "POR",
+    trophies: ["Champions League", "Mondiale", "Bundesliga"],
+    height: 193,
+    careerGoals: 5,
+    image: "images/neuer.jpg"
+  }
+  // ...aggiungi altri giocatori
+];
